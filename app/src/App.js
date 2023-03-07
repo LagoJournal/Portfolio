e23559460ef626e828d550f6c2ecbc6c2e2996c9
Agustin Lago <agustin.lago@mi.unc.edu.ar>
2023-03-07 15:44:23 -0300

    first config

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="flex flex-col width-full m-8 ">
+      <div class="flex flex-col space-y-8 w-80 p-8 bg-white rounded-3xl shadow-2xl">
+        <p class="text-4xl font-bold">Hi 👋</p>
+        <p class="w-64 h-16 text-sm">
+          Welcome to my portfolio.
+          <br />
+          Feel free to check my projects & contact info down below.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
